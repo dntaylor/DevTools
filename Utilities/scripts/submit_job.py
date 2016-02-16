0c232116cd143e36ec6ae073ff576da17c359c4d
Devin Taylor <dntaylor@wisc.edu>
2016-02-16 13:08:33 +0100

    add crab status

--- Utilities/scripts/submit_job.py
+++ Utilities/scripts/submit_job.py
@@ -10,85 +10,99 @@ import argparse
 import logging
 import os
 import sys
+import glob
 from socket import gethostname
 
 from CRABClient.ClientExceptions import ClientException
+from CRABClient.ClientUtilities import initLoggers
 from httplib import HTTPException
+import CRABClient.Commands.submit as crabClientSubmit
+import CRABClient.Commands.status as crabClientStatus
 
-
-# i want to override this
-#from CRABAPI.RawCommand import crabCommand
-
-import CRABAPI
-from CRABClient.ClientUtilities import initLoggers, flushMemoryLogger, removeLoggerHandlers
-
-# NOTE: Not included in unittests
-def crabCommand(command, *args, **kwargs):
-    """ crabComand - executes a given command with certain arguments and returns
-                     the raw result back from the client. Arguments are...
-    """
-    #Converting all arguments to a list. Adding '--' and '='
-    arguments = []
-    for key, val in kwargs.iteritems():
-        if isinstance(val, bool):
-            if val:
-                arguments.append('--'+str(key))
-        else:
-            arguments.append('--'+str(key))
-            arguments.append(val)
-    arguments.extend(list(args))
-
-    return execRaw(command, arguments)
-
-
-# NOTE: Not included in unittests
-def execRaw(command, args):
-    """
-        execRaw - executes a given command with certain arguments and returns
-                  the raw result back from the client. args is a python list,
-                  the same python list parsed by the optparse module
-    """
-    tblogger, logger, memhandler = initLoggers()
-    tblogger.setLevel(logging.INFO)
-    logger.setLevel(logging.INFO)
-    memhandler.setLevel(logging.INFO)
-
-    try:
-        mod = __import__('CRABClient.Commands.%s' % command, fromlist=command)
-    except ImportError:
-        raise CRABAPI.BadArgumentException( \
-                                        'Could not find command "%s"' % command)
-
-    try:
-        cmdobj = getattr(mod, command)(logger, args)
-        res = cmdobj()
-    except SystemExit as se:
-        # most likely an error from the OptionParser in Subcommand.
-        # CRABClient #4283 should make this less ugly
-        if se.code == 2:
-            raise CRABAPI.BadArgumentException
-    finally:
-        flushMemoryLogger(tblogger, memhandler, logger.logfile)
-        removeLoggerHandlers(tblogger)
-        removeLoggerHandlers(logger)
-    return res
+## i want to override this
+##from CRABAPI.RawCommand import crabCommand
+#
+#import CRABAPI
+#from CRABClient.ClientUtilities import initLoggers, flushMemoryLogger, removeLoggerHandlers
+#
+## NOTE: Not included in unittests
+#def crabCommand(command, *args, **kwargs):
+#    """ crabComand - executes a given command with certain arguments and returns
+#                     the raw result back from the client. Arguments are...
+#    """
+#    #Converting all arguments to a list. Adding '--' and '='
+#    arguments = []
+#    for key, val in kwargs.iteritems():
+#        if isinstance(val, bool):
+#            if val:
+#                arguments.append('--'+str(key))
+#        else:
+#            arguments.append('--'+str(key))
+#            arguments.append(val)
+#    arguments.extend(list(args))
+#
+#    return execRaw(command, arguments)
+#
+#
+## NOTE: Not included in unittests
+#def execRaw(command, args):
+#    """
+#        execRaw - executes a given command with certain arguments and returns
+#                  the raw result back from the client. args is a python list,
+#                  the same python list parsed by the optparse module
+#    """
+#    tblogger, logger, memhandler = initLoggers()
+#    tblogger.setLevel(logging.INFO)
+#    logger.setLevel(logging.INFO)
+#    memhandler.setLevel(logging.INFO)
+#
+#    try:
+#        mod = __import__('CRABClient.Commands.%s' % command, fromlist=command)
+#    except ImportError:
+#        raise CRABAPI.BadArgumentException( \
+#                                        'Could not find command "%s"' % command)
+#
+#    try:
+#        cmdobj = getattr(mod, command)(logger, args)
+#        res = cmdobj()
+#    except SystemExit as se:
+#        # most likely an error from the OptionParser in Subcommand.
+#        # CRABClient #4283 should make this less ugly
+#        if se.code == 2:
+#            raise CRABAPI.BadArgumentException
+#    finally:
+#        flushMemoryLogger(tblogger, memhandler, logger.logfile)
+#        removeLoggerHandlers(tblogger)
+#        removeLoggerHandlers(logger)
+#    return res
 
 log = logging.getLogger("submit_job")
 logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+
+
+def get_crab_workArea(args):
+    '''Get the job working area'''
+    uname = os.environ['USER']
+    scratchDir = 'data' if 'uwlogin' in gethostname() else 'nfs_scratch'
+    return '/{0}/{1}/crab_projects/{2}'.format(scratchDir,uname,args.jobName)
 
 def submit_crab(args):
     '''Create submission script for crab'''
     submit_strings = []
 
     uname = os.environ['USER']
-    scratchDir = 'data' if 'uwlogin' in gethostname() else 'nfs_scratch'
+
+    tblogger, logger, memhandler = initLoggers()
+    tblogger.setLevel(logging.INFO)
+    logger.setLevel(logging.INFO)
+    memhandler.setLevel(logging.INFO)
 
     # crab config
     from CRABClient.UserUtilities import config
 
     config = config()
     
-    config.General.workArea         = '/{0}/{1}/crab_projects'.format(scratchDir,uname)
+    config.General.workArea         = get_crab_workArea(args)
     config.General.transferOutputs  = True
     
     config.JobType.pluginName       = 'Analysis'
@@ -112,11 +126,6 @@ def submit_crab(args):
 
     config.Site.storageSite         = 'T2_US_Wisconsin'
 
-    # crab command options
-    command = 'submit'
-    commandArgs = []
-    if args.dryrun: commandArgs += ['--dryrun']
-
     # get samples
     sampleList = []
     if os.path.isfile(args.sampleList):
@@ -128,16 +137,79 @@ def submit_crab(args):
     # iterate over samples
     for sample in sampleList:
         _, primaryDataset, datasetTag, dataFormat = sample.split('/')
-        config.General.requestName = '{0}_{1}_{2}_{3}'.format(primaryDataset[:20],datasetTag[:20],dataFormat[:10],args.jobName[:30])
+        config.General.requestName = '{0}'.format(primaryDataset)
+        if dataFormat in ['MINIAOD','AOD','RAW']: # its data, also specify the aquisition period
+            config.General.requestName += '_' + datasetTag
+        # make it only 100 characters
+        config.General.requestName = config.General.requestName[:99] # Warning: may not be unique now
         config.Data.inputDataset   = sample
+        # submit the job
+        submitArgs = ['--config',config]
+        if args.dryrun: submitArgs += ['--dryrun']
         try:
             log.info("Submitting for input dataset {0}".format(sample))
-            res = crabCommand(command, config = config, *commandArgs)
+            result = crabClientSubmit.submit(logger,submitArgs)()
+            #res = crabCommand(command, config = config, *commandArgs)
         except HTTPException as hte:
             log.info("Submission for input dataset {0} failed: {1}".format(sample, hte.headers))
         except ClientException as cle:
             log.info("Submission for input dataset {0} failed: {1}".format(sample, cle))
 
+def status_crab(args):
+    '''Check jobs matching jobName'''
+    crab_dirs = []
+    if args.jobName:
+        workArea = get_crab_workArea(args)
+        crab_dirs += glob.glob('{0}/*'.format(workArea))
+    elif args.crabDirectories:
+        for d in args.crabDirectories:
+            crab_dirs += glob.glob(d)
+    else:
+        log.error("Shouldn't be possible to get here")
+
+    tblogger, logger, memhandler = initLoggers()
+    tblogger.setLevel(logging.WARNING)
+    logger.setLevel(logging.WARNING)
+    memhandler.setLevel(logging.WARNING)
+
+    statusMap = {}
+    for d in crab_dirs:
+        if os.path.exists(d):
+            statusArgs = ['--dir',d]
+            if args.verbose: statusArgs += ['--long']
+            try:
+                log.info('Retrieving status of {0}'.format(d))
+                statusMap[d] = crabClientStatus.status(logger,statusArgs)()
+            except HTTPException as hte:
+                log.info("Submission for input directory {0} failed: {1}".format(d, hte.headers))
+            except ClientException as cle:
+                log.info("Submission for input directory {0} failed: {1}".format(d, cle))
+
+    parse_crab_status(args,statusMap)
+
+def parse_crab_status(args,statusMap):
+    '''Parse the output of a crab status call'''
+    allowedStatuses = ['UPLOADED','SUBMITTED','FAILED','QUEUED','SUBMITFAILED','COMPLETED','KILLED','KILLFAILED','RESUBMITFAILED','NEW','RESUBMIT','KILL','UNKNOWN']
+    allowedStates = ['idle','running','transferring','finished','failed','unsubmitted','cooloff','killing','held']
+    statusSummary = {}
+    for status in allowedStatuses: statusSummary[status] = []
+    stateSummary = {}
+    for state in allowedStates: stateSummary[state] = 0
+    for d,summary in statusMap.iteritems():
+        status = summary['status']
+        statusSummary[status] += [d]
+        if 'jobs' in summary:
+            for j,job in summary['jobs'].iteritems():
+                stateSummary[job['State']] += 1
+    log.info('Summary')
+    for s in allowedStatuses:
+        if statusSummary[s]:
+            log.info('Status: {0}'.format(s))
+            for d in statusSummary[s]:
+                log.info('    {0}'.format(d))
+    for s in allowedStates:
+        if stateSummary[s]:
+            log.info('{0:12} : {1}'.format(s,stateSummary[s]))
 
 def submit_condor(args):
     '''Create submission script for condor'''
@@ -156,59 +228,72 @@ def parse_command_line(argv):
     # submission type
     subparsers = parser.add_subparsers(help='Submission mode')
 
-    # crab
-    parser_crab = subparsers.add_parser('crab', help='Submit jobs via crab')
-    parser_crab.add_argument('jobName', type=str, help='Job Name for submission')
-    parser_crab.add_argument('cfg', type=str, help='cmsRun config file')
-    parser_crab.add_argument('cmsRunArgs', nargs='*', 
+    # crabSubmit
+    parser_crabSubmit = subparsers.add_parser('crabSubmit', help='Submit jobs via crab')
+    parser_crabSubmit.add_argument('jobName', type=str, help='Job Name for submission')
+    parser_crabSubmit.add_argument('cfg', type=str, help='cmsRun config file')
+    parser_crabSubmit.add_argument('cmsRunArgs', nargs='*', 
         help='VarParsing arguments passed to cmsRun'
     )
 
-    parser_crab.add_argument('--sampleList', type=str,
+    parser_crabSubmit.add_argument('--sampleList', type=str,
         help='Text file list of DAS samples to submit, one per line'
     )
 
-    parser_crab.add_argument('--applyLumiMask',action='store_true',
+    parser_crabSubmit.add_argument('--applyLumiMask',action='store_true',
         help='Apply the latest golden json run lumimask to data'
     )
 
-    parser_crab.add_argument('--inputDBS', type=str, default='global',
+    parser_crabSubmit.add_argument('--inputDBS', type=str, default='global',
         choices=['global','phys01','phys02','phys03'], 
         help='DAS instance to search for input files'
     )
 
-    parser_crab.add_argument('--publish', action='store_true', help='Publish output to DBS')
+    parser_crabSubmit.add_argument('--publish', action='store_true', help='Publish output to DBS')
 
-    parser_crab.add_argument('--dryrun', action='store_true', help='Do not submit jobs')
+    parser_crabSubmit.add_argument('--dryrun', action='store_true', help='Do not submit jobs')
 
-    parser_crab.set_defaults(submit=submit_crab)
+    parser_crabSubmit.set_defaults(submit=submit_crab)
 
-    # condor
-    parser_condor = subparsers.add_parser('condor', help='Submit jobs via condor')
-    parser_condor.add_argument('jobName', type=str, help='Job Name for submission')
-    parser_condor.add_argument('cfg', type=str, help='cmsRun config file')
-    parser_condor.add_argument('cmsRunArgs', nargs='*', 
+    # crabStatus
+    parser_crabStatus = subparsers.add_parser('crabStatus', help='Check job status via crab')
+
+    parser_crabStatus_directories = parser_crabStatus.add_mutually_exclusive_group(required=True)
+    parser_crabStatus_directories.add_argument('--jobName', type=str, help='Job name from submission')
+    parser_crabStatus_directories.add_argument('--crabDirectories', type=str, nargs="*",
+        help='Space separated list of crab submission directories. Unix wild-cards allowed.',
+    )
+
+    parser_crabStatus.add_argument('--verbose', action='store_true', help='Verbose status summary')
+
+    parser_crabStatus.set_defaults(submit=status_crab)
+
+    # condorSubmit
+    parser_condorSubmit = subparsers.add_parser('condorSubmit', help='Submit jobs via condor')
+    parser_condorSubmit.add_argument('jobName', type=str, help='Job Name for submission')
+    parser_condorSubmit.add_argument('cfg', type=str, help='cmsRun config file')
+    parser_condorSubmit.add_argument('cmsRunArgs', nargs='*', 
         help='VarParsing arguments passed to cmsRun'
     )
 
-    parser_condor.add_argument('--sampleList', type=str, nargs=1,
+    parser_condorSubmit.add_argument('--sampleList', type=str, nargs=1,
         help='Text file list of DAS samples to submit, one per line'
     )
 
-    parser_condor.add_argument('--applyLumiMask',action='store_true',
+    parser_condorSubmit.add_argument('--applyLumiMask',action='store_true',
         help='Apply the latest golden json run lumimask to data'
     )
 
-    parser_condor.add_argument('--inputDBS', type=str, default='global',
+    parser_condorSubmit.add_argument('--inputDBS', type=str, default='global',
         choices=['global','phys01','phys02','phys03'], 
         help='DAS instance to search for input files'
     )
 
-    parser_condor.add_argument('-o','--outfile',type=str,default='',
+    parser_condorSubmit.add_argument('-o','--outfile',type=str,default='',
         help='Output file name to write submission commands'
     )
 
-    parser_condor.set_defaults(submit=submit_condor)
+    parser_condorSubmit.set_defaults(submit=submit_condor)
 
     return parser.parse_args(argv)
 
