e44947a540b463dd07d4a480336966e52b60e3ad
Devin Taylor <dntaylor@wisc.edu>
2016-02-16 22:31:23 +0100

    removing c++ analysis

--- MiniAnalyzer/python/WZTree_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-wzTree = cms.EDAnalyzer("WZTree",
-    fileNames = cms.vstring(),
-    inputDirectoryName = cms.string("miniTree"),
-    inputLumiName = cms.string("LumiTree"),
-    inputTreeName = cms.string("MiniTree"),
-    ouputTreeName = cms.string("WZTree"),
-)
