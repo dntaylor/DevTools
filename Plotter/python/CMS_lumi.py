b2957669b6bba9614cad007c476df8165a4e485a
Devin Taylor <dntaylor@wisc.edu>
2016-02-24 18:04:54 +0100

    fix to segfault

--- Plotter/python/CMS_lumi.py
+++ Plotter/python/CMS_lumi.py
@@ -53,8 +53,7 @@ def CMS_lumi(pad,  iPeriod,  iPosX ):
     b = pad.GetBottomMargin()
     e = 0.025
 
-    # TODO: segfault source
-    #pad.cd()
+    if pad != rt.TVirtualPad.Pad(): pad.cd()
 
     lumiText = ""
     if( iPeriod==1 ):
@@ -110,8 +109,7 @@ def CMS_lumi(pad,  iPeriod,  iPosX ):
         latex.SetTextSize(cmsTextSize*t)    
         latex.DrawLatex(l,1-t+lumiTextOffset*t,cmsText)
   
-    # TODO: segfault source
-    #pad.cd()
+    if pad != rt.TVirtualPad.Pad(): pad.cd()
 
     posX_ = 0
     if( iPosX%10<=1 ):
