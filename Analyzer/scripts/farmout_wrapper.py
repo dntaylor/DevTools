df8f2d24bd21be57144b4f62e54f23612602adf5
Devin Taylor <dntaylor@wisc.edu>
2016-02-28 09:47:33 -0600

    add options to submit different jobs

--- Analyzer/scripts/farmout_wrapper.py
+++ Analyzer/scripts/farmout_wrapper.py
@@ -4,18 +4,38 @@ import sys
 import subprocess
 import os
 
-# import wz run script
+# import run script
 from DevTools.Analyzer.runWZAnalysis import main as runWZ
+from DevTools.Analyzer.runHpp3lAnalysis import main as runHpp3l
+from DevTools.Analyzer.runHpp4lAnalysis import main as runHpp4l
+
+def parse_command_line(argv):
+    parser = argparse.ArgumentParser(description='Submit analyzers')
+
+    parser.add_argument('analysis', type=str, choices=['WZ','Hpp3l','Hpp4l'], help='Analysis to submit')
+
+    return parser.parse_args(argv)
+
+
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
+    args = parse_command_line(argv)
+
     # add on the input and output
     argv = ['--inputFileList',os.environ['INPUT'],'--outputFile',os.environ['OUTPUT']]
 
     # run the analyzer
-    status = runWZ(argv)
+    if args.analysis=='WZ':
+        status = runWZ(argv)
+    elif args.analysis=='Hpp3l':
+        status = runHpp3l(argv)
+    elif args.analysis=='Hpp4l':
+        status = runHpp4l(argv)
+    else:
+        status = 0
 
     return status
 
