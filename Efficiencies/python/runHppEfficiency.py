a2a6cc315f646072bbd30f992cd95e17ab4509b4
Devin Taylor <dntaylor@wisc.edu>
2016-02-28 11:55:30 +0100

    change input file

--- Efficiencies/python/runHppEfficiency.py
+++ Efficiencies/python/runHppEfficiency.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO, stream=sys.stderr)
 def parse_command_line(argv):
     parser = argparse.ArgumentParser(description='Run efficiencies')
 
-    parser.add_argument('--inputFiles', type=str, nargs='*', default=['/store/user/dntaylor/2016-02-27_DevTools_v1/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/2016-02-27_DevTools_v1/160227_142430/0000/miniTree_1.root'], help='Input files')
+    parser.add_argument('--inputFiles', type=str, nargs='*', default=['/store/user/dntaylor/2016-02-27_DevTools_v1/HPlusPlusHMinusMinusHTo4L_M-500_13TeV-pythia8/2016-02-27_DevTools_v1/160227_142623/0000/miniTree_1.root'], help='Input files')
     parser.add_argument('--inputFileList', type=str, default='', help='Input file list')
     parser.add_argument('--outputFile', type=str, default='hppEfficiency.root', help='Output file')
 
