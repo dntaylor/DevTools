3082830a3e04daf7baf9730d6e2ba7b0913e28d6
Devin Taylor <dntaylor@wisc.edu>
2016-03-15 05:02:18 -0500

    remove 76X specific stuff

--- recipe/recipe.sh
+++ recipe/recipe.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # CMSSW packages
-pushd $CMSSW_BASE/src
-git cms-merge-topic -u matteosan1:smearer_76X
-popd
+
+# electron smear corrections not yet availabel in 80X
+#pushd $CMSSW_BASE/src
+#git cms-merge-topic -u matteosan1:smearer_76X
+#popd
 
 # python utilities
 git clone --branch 15.0.0 https://github.com/pypa/virtualenv.git $CMSSW_BASE/src/DevTools/recipe/virtualenv
