7d1347a616d705019757bef315feb437196a426c
Devin Taylor <dntaylor@wisc.edu>
2016-08-08 02:30:09 -0500

    backport 80X

---
+++ recipe/updateSubmodules.sh
@@ -0,0 +1,29 @@
+# utilities
+pushd $CMSSW_BASE/src/DevTools/Utilities
+git pull
+popd
+
+# limits
+pushd $CMSSW_BASE/src/DevTools/Limits
+git pull
+popd
+
+# plotter
+pushd $CMSSW_BASE/src/DevTools/Plotter
+git pull
+popd
+
+# analyzer
+pushd $CMSSW_BASE/src/DevTools/Analyzer
+git pull
+popd
+
+# ntuplizer
+pushd $CMSSW_BASE/src/DevTools/Ntuplizer
+git pull
+popd
+
+# tag and probe
+pushd $CMSSW_BASE/src/DevTools/TagAndProbe
+git pull
+popd
