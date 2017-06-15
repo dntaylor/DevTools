a277c4eab702450927825690a01af98c413378a1
Devin Taylor <dntaylor@wisc.edu>
2017-06-15 12:20:39 -0500

    new branch setup

--- recipe/setupSubmodules.sh
+++ recipe/setupSubmodules.sh
@@ -1,32 +1,40 @@
 #!/usr/bin/env bash
 
+# submodules
+
 # utilities
 pushd $CMSSW_BASE/src/DevTools/Utilities
+git checkout master
 git pull
 popd
 
 # limits
 pushd $CMSSW_BASE/src/DevTools/Limits
+git checkout master
 git pull
 popd
 
 # plotter
 pushd $CMSSW_BASE/src/DevTools/Plotter
+git checkout master
 git pull
 popd
 
 # analyzer
 pushd $CMSSW_BASE/src/DevTools/Analyzer
+git checkout master
 git pull
 popd
 
 # ntuplizer
 pushd $CMSSW_BASE/src/DevTools/Ntuplizer
+git checkout 92X
 git pull
 popd
 
 # tag and probe
 pushd $CMSSW_BASE/src/DevTools/TagAndProbe
+git checkout 92X
 git pull
 popd
 
